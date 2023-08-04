c3a499ef64cdd5113a942ee9e5af483a5a520717
cjcropp <cjcropp16@gmail.com>
2023-08-03 21:57:17 -0700

    Add color selector color randomizer
    Create an input with a color wheel for the user to be able to choose which color to draw, then create a button to randomize rgb for each cell hovered over

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let slider = document.getElementById("sizeRange");
 let output = document.getElementById("demo")
 output.textContent = slider.value;
+let color = document.getElementById("colorSelector")
 
 let gridContainer = document.querySelector("#gridContainer");
-/*let grid = slider.value
-let total = grid * grid*/
 
 function createGrid() {
     let grid = slider.value
@@ -13,8 +12,8 @@ function createGrid() {
         const gridItem = document.createElement('div');
         gridContainer.append(gridItem);
         gridItem.classList.add('cell');
-        gridItem.setAttribute("id", i + 1)
-        gridItem.addEventListener("mouseover", () => gridItem.style.setProperty(`background-color`, `red`));
+        gridItem.setAttribute("id", i + 1);
+        gridItem.addEventListener("mouseover", () => gridItem.style.setProperty(`background-color`, `${color.value}`));
         gridItem.style.height = "auto";
         gridItem.style.width = "auto";
     };
@@ -25,23 +24,40 @@ function createGrid() {
 createGrid();
 
 let cell = document.getElementsByClassName('cell');
-console.log(cell);
 
 function addColor () {
-    cell.style.setProperty(`color`, `red`);
+    cell.style.setProperty(`color`, color.value);
 };
 
-/*while (gridContainer.lastChild) {
-    gridContainer.lastChild.remove();
-};*/
+
+color.oninput = function () {
+    for (let i = 1; i < cell.length+1; i++) {
+        let gridItem = document.getElementById(`${i}`)
+        gridItem.addEventListener("mouseover", function() { 
+            gridItem.style.setProperty(`background-color`, `${color.value}`)
+        });
+    };
+};
 
 slider.oninput = function() {
     output.textContent = this.value;
-    /*let grid = slider.value;
-    let total = grid * grid;*/
     while (gridContainer.lastChild) {
         gridContainer.lastChild.remove();
     };
     createGrid();
 };
 
+function getRandomColor () {
+    let color = '#'+Math.random().toString(16).substr(-6);
+    return color;
+};
+    
+const rgbButton = document.getElementById('rgbButton');
+rgbButton.onclick = function () {
+    for (let i = 1; i < cell.length+1; i++) {
+        let gridItem = document.getElementById(`${i}`)
+        gridItem.addEventListener("mouseover", function() { 
+            gridItem.style.setProperty(`background-color`, `${getRandomColor()}`)
+        });
+    };
+};
